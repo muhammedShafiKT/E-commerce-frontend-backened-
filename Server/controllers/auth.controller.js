c53a88d0f70fea0ca7105951e3cbfcd844a96452
muhammedshafikt <muhammedshafikt85@gmail.com>
2026-06-05 10:17:09 +0530

    brevo  issue

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -2,7 +2,9 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import SibApiV3Sdk from "@getbrevo/brevo";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const SibApiV3Sdk = require("@getbrevo/brevo");
 dotenv.config();
 
 // Brevo setup
